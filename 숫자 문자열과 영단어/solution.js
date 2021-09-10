e7389f6f1fd35475d9a1416b1f327f7f739abd17
Gayun Lee <gayunlee11@gmail.com>
2021-09-10 10:42:15 +0900

    미완성

--- 숫자 문자열과 영단어/solution.js
+++ 숫자 문자열과 영단어/solution.js
@@ -1,14 +1,14 @@
 function solution(s){
+  let matchEl = [];
   let arr = ["zero","one","two","three","four","five","six","seven","eight","nine","ten"]
-for(let i=0; i<arr.length;i++){
+for(let i=0; i<arr.length;i++) {
   if(s1.match(arr[i])){
-     let first = s1.indexOf(arr[i])
-      let strlength = arr[i].length
-      console.log(first, strlength)
-      let y = s1.replace(arr[i],[i])
-
-      s1.replace("one",1)
-      console.log(y)
-     }
-    }
+      matchEl.push(arr[i])
   }
+}
+  for(let i = 0; i < matchEl.length; i++) {
+    const replaceStr = s1.replace(matchEl[i],)
+  }
+     let y = s1.replace(arr[i],[i])
+}
+  
