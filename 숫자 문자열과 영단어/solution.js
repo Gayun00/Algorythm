f77ec5c6837dcc63bdb15b988a2d35f7b1ce6b67
Gayun Lee <gayunlee11@gmail.com>
2021-09-10 10:28:17 +0900

    ver.1

---
+++ 숫자 문자열과 영단어/solution.js
@@ -0,0 +1,14 @@
+function solution(s){
+  let arr = ["zero","one","two","three","four","five","six","seven","eight","nine","ten"]
+for(let i=0; i<arr.length;i++){
+  if(s1.match(arr[i])){
+     let first = s1.indexOf(arr[i])
+      let strlength = arr[i].length
+      console.log(first, strlength)
+      let y = s1.replace(arr[i],[i])
+
+      s1.replace("one",1)
+      console.log(y)
+     }
+    }
+  }
