24f3870799702b8da32b9a63ba60d10db662c28a
Gayun Lee <gayunlee11@gmail.com>
2021-09-12 22:16:05 +0900

    다른 풀이

--- 음양 더하기/solution.js
+++ 음양 더하기/solution.js
@@ -1,22 +1,27 @@
+// function solution(absolutes, signs) {
+//   const positiveNum = [];
+//   const negativeNum = [];
+
+//   for(let i = 0; i<signs.length; i++) {
+//     if(signs[i] === true) {
+//       positiveNum.push(absolutes[i])
+//     } else {
+//       negativeNum.push(absolutes[i])
+//     }
+//   }
+
+//   const sumPos = positiveNum.reduce((sum,el) => {
+//     return sum+el
+//   },0)
+//   const sumNeg = negativeNum.reduce((sum,el) => {
+//     return sum+el
+//   },0)
+
+//   const answer = sumPos - sumNeg
+//   return answer
+// }
+
 function solution(absolutes, signs) {
-  const positiveNum = [];
-  const negativeNum = [];
 
-  for(let i = 0; i<signs.length; i++) {
-    if(signs[i] === true) {
-      positiveNum.push(absolutes[i])
-    } else {
-      negativeNum.push(absolutes[i])
-    }
-  }
-
-  const sumPos = positiveNum.reduce((sum,el) => {
-    return sum+el
-  },0)
-  const sumNeg = negativeNum.reduce((sum,el) => {
-    return sum+el
-  },0)
-
-  const answer = sumPos - sumNeg
-  return answer
+  return absolutes.reduce((acc, val, i) => acc + (val * (signs[i] ? 1 : -1)), 0);
 }
