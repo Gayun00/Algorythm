91623c63de80734771a872780f5e62197d6e08c5
Gayun Lee <gayunlee11@gmail.com>
2021-09-17 07:45:44 +0900

    다른풀이

--- 없는 숫자 더하기/solution.js
+++ 없는 숫자 더하기/solution.js
@@ -13,3 +13,24 @@ function solution(numbers) {
       return sum + curr;
   });
 }
+
+/**
+ * 다른풀이 1.
+ *
+ * function solution(numbers) {
+    let cnt = 0;
+    for(let i=0; i<10; i++){
+        if(!(numbers.includes(i))) cnt+= i
+    }
+    return cnt
+
+}
+ *
+ * 다른풀이 2.
+ *
+ * function solution(numbers) {
+    return 45 - numbers.reduce((cur, acc) => cur + acc, 0);
+}
+ *
+ *
+ */
