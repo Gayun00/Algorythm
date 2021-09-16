248d80dd454f09345137aeafe84b99d5f93e43c0
Gayun Lee <gayunlee11@gmail.com>
2021-09-16 11:51:15 +0900

    localCompare

--- 문자열 내 마음대로 정렬하기/solution.js
+++ 문자열 내 마음대로 정렬하기/solution.js
@@ -1,4 +1,7 @@
 function solution(strings, n) {
-  const answer =  strings.sort((s1, s2) => s1[n] === s2[n] ? s1.localeCompare(s2) : s1[n].localeCompare(s2[n]));
-  return answer
+  const answer =  strings.sort(
+    (s1, s2) => s1[n] === s2[n]
+      ? s1.localeCompare(s2)
+      : s1[n].localeCompare(s2[n]));
+  return answer;
 }
