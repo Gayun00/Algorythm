c584cb3b21351c3a2e564aadd74b34e721efee41
Gayun Lee <gayunlee11@gmail.com>
2021-09-18 08:45:43 +0900

    ver2: split, map, join

--- 이상한 문자 만들기/solution.js
+++ 이상한 문자 만들기/solution.js
@@ -1,20 +1,14 @@
 function solution(s) {
-  const arr = [];
-  const sliced = s.split(' ')
-  sliced.forEach((el) => {
-    for(i=0; i<el.length; i++) {
-      if(i%2 ===0) {
-        const upper = el[i].toUpperCase();
-        arr.push(upper)
-      } else {
-        arr.push(el[i])
+  return s.split(' ').map(word => {
+      let result = '';
+      for(let i = 0; i < word.length; i++) {
+          if(i%2) {
+              result += word[i].toLowerCase();
+          } else {
+              result += word[i].toUpperCase();
+          }
       }
-    }
-    con
-  })
-  return arr.join('')
+      return result;
+  }).join(' ');
 }
-//TrY HeLlO WoRlD 가 나와야 하는데,
-//공백을 이미 잘라서 다시 넣어두기가 힘들다.
-//sliced 원본 배열에는 변형이 없고, arr에는 단순히 각 글자를 push해서
-//forEach문이 돌면서 단어별로 공백을 추가할 수 없다.
+//원본배열에 적용시키기 힘들 때는 map을 사용하자.
