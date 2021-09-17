7e94cfc929030ae6ef2abb53c01c7c7916994afd
Gayun Lee <gayunlee11@gmail.com>
2021-09-18 08:07:30 +0900

    soltuion 2: Math.min(...arr)

--- 제일 작은 수 제거하기/solution.js
+++ 제일 작은 수 제거하기/solution.js
@@ -1,11 +1,11 @@
+//정렬을 사용한 방법은 불가능 해 수정
 function solution(arr) {
-    let arr2 = arr;
-    const sorted = arr2.sort((a,b)=>b-a);
-    const removed = sorted.splice(arr2.length-1,1)
-    if(arr2.length !==0) {
-        return arr2;
-    } else{
-        arr2 = [-1];
-        return arr2;
-    }      
+  let arr2 = arr;
+  const min = Math.min(...arr2);
+  const minIndex = arr2.indexOf(min);
+  arr2.splice(minIndex,1)
+
+  if(arr2.length === 0) return [-1];
+
+return arr2;
 };
