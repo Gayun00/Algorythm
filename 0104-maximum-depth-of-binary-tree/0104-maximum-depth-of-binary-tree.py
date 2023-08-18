55b0e894df035a613d648dc49acd2c12ed598629
gygy <67543454+Gayun00@users.noreply.github.com>
2023-08-18 09:32:47 +0900

    Time: 44 ms (87.98%), Space: 17.6 MB (90.14%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -1,25 +1,32 @@
+from collections import deque
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-from collections import deque
-
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        max_depth = 0
         if root is None:
-            return max_depth
-        q = deque()
-        q.append((root, 1))
-        while q:
-            cur_node, cur_depth = q.popleft()
-            max_depth = max(max_depth, cur_depth)
-            if cur_node.left:
-                q.append((cur_node.left, cur_depth + 1))
-            if cur_node.right:
-                q.append((cur_node.right, cur_depth + 1))
-        return max_depth
+            return 0
+            
+        
+        queue = deque()
+        queue.append((1, root)) # (depth, rootNode)
+        max_depth = 0
+        
+        while queue:
 
+            cur_depth, cur_node = queue.popleft()
+            max_depth = max(max_depth, cur_depth)
+            
+            if cur_node.left:
+                queue.append((cur_depth + 1, cur_node.left))
+            if cur_node.right:
+                queue.append((cur_depth + 1, cur_node.right))
+                
+
+        
+        return max_depth
+        
