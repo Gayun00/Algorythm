5d621a985dcea2a477d99b1ee1131124486da70f
gygy <67543454+Gayun00@users.noreply.github.com>
2023-08-24 10:05:15 +0900

    Time: 70 ms (45.77%), Space: 18.9 MB (5.82%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,12 +1,27 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
-        min_costs = [0] * (n + 1)
-        
-        for i in range(2, n + 1):
-            min_costs[i] = min(min_costs[i-1]+ cost[i-1],min_costs[i-2]+ cost[i-2])
+#         1. dp bottom-up
 
-        return min_costs[n]
+#         n = len(cost)
+#         min_costs = [0] * (n + 1)
+        
+#         for i in range(2, n + 1):
+#             min_costs[i] = min(min_costs[i-1]+ cost[i-1],min_costs[i-2]+ cost[i-2])
+
+#         return min_costs[n]
+
+#       2. dp top-down
+        memo = {}
+        
+        def minCosts(n):
+            if n == 0 or n == 1:
+                return 0
+            if n not in memo:
+                memo[n] = min(minCosts(n-1) + cost[n-1], minCosts(n-2) + cost[n-2])
+            return memo[n]
+        
+        minCosts(len(cost))
+        return memo[len(cost)]
         
         
         
