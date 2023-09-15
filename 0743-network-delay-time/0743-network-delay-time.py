ab1a748fe147cc7e64239a34ebff871ce82b3bc7
gygy <67543454+Gayun00@users.noreply.github.com>
2023-09-15 18:33:44 +0900

    Time: 468 ms (22.38%), Space: 18.6 MB (67.92%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,39 +1,37 @@
-from collections import defaultdict
-import heapq
-
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        graph = defaultdict(list)
-       # 기본 그래프 만들기
+        # 다익스트라 그래프 만들기
         graph = defaultdict(list)
 
-        for time in times:
-            graph[time[0]].append((time[2], time[1]))
+        for source, target, time in times:
+            graph[source].append((time, target))
 
+        print(graph)
+
+        # 최소 비용을 구해야 하니까, 비용 기준 우선순위 큐를 만들기
         pq = []
-        costs = {}
-
-        # 우선순위큐로 pq에 첫번째 값 넣기. 정렬된 상태로 추가 및 삭제
-        #cost가 적은 순으로 우선순위 정렬이 되어야 한다. 
-        # 그래야 최소 비용 기준으로 계산이 가능
         heapq.heappush(pq, (0, k))
 
+        # 비용 정보들을 기록해둘 costs 객체
+        costs = {}
+
+        # pq를 돌면서 비용 합계 구하기
         while pq:
             cur_cost, cur_node = heapq.heappop(pq)
-            # 이동 총 비용 계산
+            # 새로운 노드라면 비용 기록. 체크용
             if cur_node not in costs:
                 costs[cur_node] = cur_cost
-                # 다음 큐에 푸시 - 해당 노드를 아직 순회하지 않았으니 순회할 것들을 큐에 푸시해둔다.
-                # 단, 현재까지 이동한 것에 대한 비용은 더해준다.
-                for cost,next_node in graph[cur_node]:
-                    next_cost = cur_cost + cost
+                # 현재 노드에서 연결되어 있는 다른 노드들을 순차적으로 방문해야 되므로, 연결된 다른 노드들 방문 예약
+                # 여기에서 비용의 합계가 필요하므로 비용은 합산해서 pq에 넣어준다.
+                for cost, next_node in graph[cur_node]:
+                    next_cost = cost + cur_cost
                     heapq.heappush(pq, (next_cost, next_node))
 
-        # 계산이 끝난 후에도 모든 노드가 시그널을 받지 못했다면 -1을 반환한다.
-        # 순회하면서 Costs에 기록해두었으니 Costs에 없다는 것은 시그널을 받지 못했다는 것을 의미한다.
-        for node in range(1, n + 1):
-            if node not in costs:
+        # 전체 노드들을 순회하면서 지금까지 방문한 서로 연결된 노드에 모두 포함되는지 확인한다.
+        # 연결되지 않은 노드가 있다면, 즉 방문하지 못한 노드가 있다면 -1을 반환한다.
+        for i in range(1, n + 1):
+            if i not in costs:
                 return -1
 
-        # 각 노드를 순회하는데 필요한 총 비용은 costs의 가장 최댓값과 같다.
+        # 가장 최소 비용으로 기록한 비용의 합계중 가장 큰 값을 반환해야 가장 먼 거리의 n, 최종 목적지인 노드까지의 비용을 구할 수 있다.
         return max(costs.values())
