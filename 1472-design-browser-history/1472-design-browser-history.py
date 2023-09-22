0ab099a9ce1c3c36a2d929d8109a7c923e5d03e9
gygy <67543454+Gayun00@users.noreply.github.com>
2023-09-23 08:36:24 +0900

    Time: 247 ms (49.40%), Space: 19 MB (41.28%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -1,28 +1,31 @@
-class ListNode():
-    def __init__(self, val=0, prev=None, next=None):
+class ListNode:
+    def __init__(self, val=0,prev=None, next=None):
         self.val = val
-        self.prev = prev
-        self.next = next
+        self.prev= prev
+        self.next= next
+    
+
 
 class BrowserHistory:
 
     def __init__(self, homepage: str):
         self.head = self.current = ListNode(val=homepage)
+        
 
     def visit(self, url: str) -> None:
         self.current.next = ListNode(val=url, prev=self.current)
-        self.current = self.current.next 
+        self.current = self.current.next
         return None
-    
+
     def back(self, steps: int) -> str:
-        while steps > 0 and self.current.prev != None:
+        while steps and self.current.prev is not None:
             steps-=1
             self.current = self.current.prev
         return self.current.val
 
     def forward(self, steps: int) -> str:
-        while steps > 0 and self.current.next != None:
-            steps-=1
+        while steps and self.current.next is not None:
+            steps -=1
             self.current = self.current.next
         return self.current.val
 
