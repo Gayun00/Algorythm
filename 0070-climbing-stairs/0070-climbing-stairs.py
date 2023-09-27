c0c1bc932ae72a7c0d76f30857a9bb0eeb5867e2
gygy <67543454+Gayun00@users.noreply.github.com>
2023-09-27 12:25:14 +0900

    Time: 33 ms (83.29%), Space: 16.3 MB (57.95%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,15 +1,29 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dict = {}
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
-        dict[1] = 1
-        dict[2] = 2
+#         topdown
+        dict= {}
         
-        for i in range(3, n+1):
-            dict[i] = dict[i-1] + dict[i-2]
-        return dict[n]
+        def dp(m):
+            if m == 1:
+                return 1
+            if m == 2:
+                return 2
+            if m not in dict:
+                dict[m] =  dp(m-1) + dp(m-2)
+            return dict[m]
+        return dp(n)
+    
+#         bottomup
+#         dict = {}
+#         if n == 1:
+#             return 1
+#         if n == 2:
+#             return 2
+#         dict[1] = 1
+#         dict[2] = 2
+        
+#         for i in range(3, n+1):
+#             dict[i] = dict[i-1] + dict[i-2]
+#         return dict[n]
             
         
