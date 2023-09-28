ccc6fe605f43a35ff93ac7ffcaeb21757370b6d2
gygy <67543454+Gayun00@users.noreply.github.com>
2023-09-28 10:50:54 +0900

    Time: 40 ms (80.34%), Space: 16.5 MB (30.85%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -3,17 +3,19 @@ from functools import cache
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         @cache
+        
         def dfs(startIdx):
-            if startIdx == len(s):
-                return True
             for word in wordDict:
-                if s[startIdx: startIdx + len(word)] == word:
-                    if dfs(startIdx + len(word)):
+                n = len(word)
+                endIdx = startIdx + n
+                
+                if startIdx == len(s):
+                    return True
+                
+                if s[startIdx:endIdx]  == word:
+                    if dfs(endIdx):
                         return True
             return False
-                    
-        return dfs(0)
+                
             
-                
-                
-        
+        return dfs(0)
