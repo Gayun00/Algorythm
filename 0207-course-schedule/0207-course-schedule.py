21c19aa5f64fec82acae8a6b7eb6ef05017756e6
gygy <67543454+Gayun00@users.noreply.github.com>
2023-10-20 10:12:11 +0900

    Time: 102 ms (40.00%), Space: 19.3 MB (15.90%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,28 @@
+class Status():
+    INITIAL = 1
+    IN_PROGRESS = 2
+    FINISHED = 3
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = {i: [] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            graph[crs].append(pre)
+        statuses = {i: Status.INITIAL for i in range(numCourses)}
+
+
+        def can_finish(crs):
+            if statuses[crs] == Status.IN_PROGRESS:
+                return False
+            if statuses[crs] == Status.FINISHED:
+                return True
+
+            statuses[crs] = Status.IN_PROGRESS
+            for pre in graph[crs]:
+                if not can_finish(pre):
+                    return False
+            statuses[crs] = Status.FINISHED
+            return True
+        
+        return all(can_finish(src) for src in graph)
