cdf0ddf9d4f3e7796fc25575d631e1bffd6afd06
gygy <67543454+Gayun00@users.noreply.github.com>
2023-10-26 11:21:00 +0900

    Time: 450 ms (16.53%), Space: 18.5 MB (73.20%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,31 +1,30 @@
+from heapq import heappop, heappush
 from collections import defaultdict
-import heapq
 
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-#         다익스트라. 그래프 만들기
         graph = defaultdict(list)
         pq = []
         costs = {}
-        
-        for source, target, time in times:
-            graph[source].append((time, target))
-            
-        heapq.heappush(pq, (0, k))
+        for time in times:
+            [source, target, distance] = time
+            graph[source].append((distance, target))
+        heap = heapq.heappush(pq, (0, k))
         
         while pq:
-            cur_cost, cur_node = heapq.heappop(pq)
+            cur_distance, cur_node = heapq.heappop(pq)
             if cur_node not in costs:
+                costs[cur_node] = cur_distance
                 next_cost = 0
-                costs[cur_node] = cur_cost
                 
-                for cost, next_node in graph[cur_node]:
-                    next_cost = cost+cur_cost
-                    heapq.heappush(pq, (next_cost, next_node))
-                    
-                    
-        for i in range(1, n+1):
-            if i not in costs:
+                for next_distance, next_node in graph[cur_node]:
+                    heapq.heappush(pq, (next_distance + cur_distance, next_node))    
+        
+        for i in range(1, n + 1):
+            if not i in costs:
                 return -1
             
+        
         return max(costs.values())
+        
+        
