bb8fffe7f614af10a5c033da4d9670a706d88e0d
gygy <67543454+Gayun00@users.noreply.github.com>
2023-10-27 16:17:32 +0900

    [level 3] Title: 등산코스 정하기, Time: 7527.82 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
@@ -3,32 +3,34 @@ from collections import defaultdict
 
 def solution(n, paths, gates, summits):
     graph = defaultdict(set)
-    for source,target,time in paths:
-        graph[source].add((time,target))
-        graph[target].add((time,source))
+    for source, target, time in paths:
+        graph[source].add((time, target))
+        graph[target].add((time, source))
         
-    intensities = [float('inf')] * (n + 1)
-    heap = []
-    
+    intensities = [float('inf')] * (n+1)
+
+    que = []
     for gate in gates:
         intensities[gate] = 0
-        heappush(heap,(0, gate))
+        heappush(que, (0, gate))
         
-    while heap:
-        cur_time, cur_node = heappop(heap)
+    while que:
+        cur_time, cur_node = heappop(que)
         if intensities[cur_node] < cur_time or cur_node in summits:
             continue
+        
         for next_time, next_node in graph[cur_node]:
-            time = max(cur_time, next_time)
-            if intensities[next_node] > time:
-                intensities[next_node] = time
-                heappush(heap,(time, next_node))
-                
-    summits = set(summits)
+            intensity = max(next_time, cur_time)
+            
+            if intensities[next_node] > intensity:
+                intensities[next_node] = intensity
+                heappush(que, (intensity, next_node))
+        
     answer = [-1, float('inf')]
     
+    summits = set(summits)
     for summit in sorted(summits):
         if intensities[summit] < answer[1]:
             answer = [summit, intensities[summit]]
+    
     return answer
-                
