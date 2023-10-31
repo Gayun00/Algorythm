46956b30c7aa39f8b0dc5e35484da6b45af7e85b
gygy <67543454+Gayun00@users.noreply.github.com>
2023-10-31 16:44:02 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 5.84 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
@@ -6,23 +6,23 @@ def solution(alp, cop, problems):
         max_alp = max(max_alp, problem[0])
         max_cop = max(max_cop, problem[1])
         
-    dp = [[float('inf')] * (max_cop + 1) for _ in range(max_alp+1)]
+    dp = [[float('inf')] * (max_cop + 1) for _ in range((max_alp + 1))]
+    
     alp = min(alp, max_alp)
     cop = min(cop, max_cop)
     dp[alp][cop] = 0
-        
-    for row in range(alp, max_alp+1):
-        for col in range(cop, max_cop+1):
-            if row < max_alp:
-                dp[row + 1][col] = min(dp[row+1][col], dp[row][col]+1)
-            if col < max_cop:
-                dp[row][col + 1] = min(dp[row][col+1], dp[row][col]+1)
+    
+    for a in range(alp, max_alp + 1):
+        for c in range(cop, max_cop + 1):
+            if a < max_alp:
+                dp[a+1][c] = min(dp[a+1][c], dp[a][c] + 1)
+            if c < max_cop:
+                dp[a][c+1] = min(dp[a][c+1], dp[a][c] + 1)
             
             for alp_req, cop_req, alp_rwd, cop_rwd, cost in problems:
-                if  row >= alp_req and col >= cop_req:
-                    new_alp = min(row+alp_rwd, max_alp)  
-                    new_cop = min(col+cop_rwd, max_cop)
-                    dp[new_alp][new_cop] = min(dp[new_alp][new_cop], dp[row][col] + cost)
-    
-    
+                if a >= alp_req and  c >= cop_req:
+                    new_alp = min(a + alp_rwd, max_alp)
+                    new_cop = min(c + cop_rwd, max_cop)
+                    dp[new_alp][new_cop] = min(dp[new_alp][new_cop], dp[a][c] + cost)
     return dp[max_alp][max_cop]
+                    
