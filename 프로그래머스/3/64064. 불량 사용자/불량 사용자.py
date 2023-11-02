bdea483967b85497813574ddfa564bf7040a5a07
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-02 09:54:01 +0900

    [level 3] Title: 불량 사용자, Time: 3.43 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -1,23 +1,26 @@
+import itertools
 import re
-from itertools import permutations
-
 
 def solution(user_id, banned_id):
     n = len(banned_id)
-    banned_id = [i.replace("*", ".") for i in banned_id]
+    user_comb = itertools.permutations(user_id, n)
+    converted_banned = [string.replace('*','.') for string in banned_id]
     answer = []
-
-    for i in permutations(user_id, n):
-        lst = list(i)
+        
+    for comb in user_comb:
+        comb_arr = list(comb)
         flag = True
-        for j in range(n):
-            if re.match(banned_id[j], lst[j]) and (len(banned_id[j]) == len(lst[j])):
+        
+        for i in range(n):
+            if re.match(converted_banned[i], comb_arr[i]) and len(converted_banned[i]) == len(comb_arr[i]):
                 continue
             else:
                 flag = False
                 break
+        
         if flag:
-            if sorted(lst) not in answer:
-                answer.append(sorted(lst))
-
+            if sorted(comb_arr) not in answer:
+                answer.append(sorted(comb_arr))
+                
     return len(answer)
+                
