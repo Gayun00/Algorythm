25614888e4963babf81ae080bcea4f51ab8ee741
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-02 13:07:21 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.61 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/README.md
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [카카오 인턴] 키패드 누르기 - 67256 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.64 ms
+메모리: 10.2 MB, 시간: 0.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 0일 16:55:14
+2023년 11월 4일 13:7:20
 
 ### 문제 설명
 
--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,55 +1,56 @@
-import math
-
-def calculate_distance(start, end):
+def checkDistance(start, end):
     x1, y1 = start
     x2, y2 = end
     
     return abs(x2 - x1) + abs(y2 - y1)
 
 def solution(numbers, hand):
+    result = ""
     left_keys = [1,4,7]
     right_keys = [3,6,9]
-    left = 10
-    right = 11
-    answer = ''
+    cur_left = "*"
+    cur_right="#"
     
     keypad = {
-        1: (0,0),
-        2: (0,1),
-        3: (0,2),
-        4: (1,0),
-        5: (1,1),
-        6: (1,2),
-        7: (2,0),
-        8: (2,1),
-        9: (2,2),
-        10: (3,0),
-        0: (3,1),
-        11: (3,2)
+        1:(0,0),
+        2:(0,1),
+        3:(0,2),
+        4:(1,0),
+        5:(1,1),
+        6:(1,2),
+        7:(2,0),
+        8:(2,1),
+        9:(2,2),
+        "*":(3,0),
+        0:(3,1),
+        "#":(3,2)
     }
     
-    for num in numbers:
-        if num in left_keys:
-            left = num
-            answer += "L"
-        elif num in right_keys:
-            right = num
-            answer += "R"
-        else:
-            keypad_row, keypad_col = keypad[num]
-            left_distance = calculate_distance(keypad[left],keypad[num])
-            right_distance = calculate_distance(keypad[right],keypad[num])
-            if left_distance < right_distance:
-                left = num
-                answer += "L"
-            elif left_distance > right_distance:
-                right = num
-                answer += "R"
+    for number in numbers:
+        if number in left_keys:
+            cur_left = number
+            result += "L"
+            continue
+        elif number in right_keys:
+            cur_right = number
+            result += "R"
+            continue
+            
+        if checkDistance(keypad[cur_left],keypad[number]) > checkDistance(keypad[cur_right],keypad[number]):
+            cur_right = number
+            result += "R"
+        elif checkDistance(keypad[cur_left],keypad[number]) == checkDistance(keypad[cur_right],keypad[number]):
+            if hand == "right":
+                cur_right = number
+                result += "R"
             else:
-                if hand == "right":
-                    right = num
-                    answer += "R"
-                else:
-                    left = num
-                    answer += "L"
-    return answer
+                cur_left = number
+                result += "L"
+        else:
+            cur_left = number
+            result += "L"
+            
+    return result
+            
+
+ 
