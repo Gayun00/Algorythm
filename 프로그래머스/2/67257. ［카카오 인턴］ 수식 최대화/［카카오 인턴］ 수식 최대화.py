4ec3eeaacb6e2a2a7453c3310b572c9069c72998
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-02 13:26:45 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.86 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -1,18 +1,24 @@
-import re
 import itertools
+import re
 
 def solution(expression):
-    expression = re.split('([-+*]{1})',expression)
-    operators = list(itertools.permutations(["+","-","*"], 3))
+    combinations = list(itertools.permutations(["+","-","*"],3))
+    splited = re.split("([-+*]{1})", expression)
     results = []
-    
-    for operator in operators:
-        exp = expression[:]
-        for op in operator:
-            while op in exp:
-                index = exp.index(op)
-                exp[index-1] = str(eval(exp[index-1] + op + exp[index + 1]))
-                del exp[index:index+2]
-        results.append(abs(int(exp[0])))
+
+    for combination in combinations:
+        comb = list(combination)
+        new_expression = splited[:]
+        
+        for sign in comb:
+            while sign in new_expression:
+                index = new_expression.index(sign)
+                result = str(eval(new_expression[index-1] + sign + new_expression[index+1]))
+                del new_expression[index:index+2]
+                new_expression[index-1] = result
+                
+        results.append(abs(int(new_expression[0])))
+        
     return max(results)
-    
+        
+                
