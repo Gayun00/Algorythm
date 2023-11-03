35292e9b58b67febb496c31b28776648eb6befd4
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-03 16:19:28 +0900

    Time: 277 ms (75.46%), Space: 18.5 MB (98.97%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,33 +2,36 @@ from collections import deque
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        number_of_islands = 0
-        m = len(grid)
-        n = len(grid[0])
-        visited = [[False] * n for _ in range(m)]
+        ROWS, COLS = len(grid), len(grid[0])
+        visited = [[False] * COLS for _ in range(ROWS)]
+        answer = 0
         
-        def bfs(x,y):
-            dx = [-1, 0 , 1, 0]
-            dy = [0, 1, 0, -1]
-            visited[x][y] = True
-            queue = deque()
-            queue.append((x, y))
+        def bfs(row, col):
+            que = deque()
+            directions = [(0,1),(0,-1),(1,0),(-1,0)]
+            visited[row][col] = True
+            que.append((row, col))
             
-            while queue:
-                cur_x, cur_y = queue.popleft()
-                for d in range(4):
-                    next_x = cur_x + dx[d]
-                    next_y = cur_y + dy[d]
-                 
-                    if next_x >= 0 and next_y >= 0 and next_x < m and next_y < n:
-                        if grid[next_x][next_y] == "1" and not visited[next_x][next_y]:
-                            visited[next_x][next_y] = True
-                            queue.append((next_x, next_y))
+
+            while que:
+                cur_row, cur_col = que.popleft()
+
+                for r, c in directions:
+                    next_row = cur_row + r
+                    next_col = cur_col + c
+
+                    if next_row <0 or next_row >=ROWS or next_col < 0 or next_col >= COLS or visited[next_row][next_col] or grid[next_row][next_col] =="0":
+                        continue
+                    visited[next_row][next_col] = True
+                    que.append((next_row, next_col))
+    
+        for row in range(ROWS):
+            for col in range(COLS):
+                if grid[row][col] == "1" and not visited[row][col]:
+                    bfs(row, col)
+                    answer += 1
                     
+        return answer
+                
+            
         
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == "1" and not visited[i][j]:
-                    bfs(i, j)
-                    number_of_islands += 1
-        return number_of_islands
