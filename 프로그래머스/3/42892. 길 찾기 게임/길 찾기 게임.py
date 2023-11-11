2af5f096dd7dda90872b99d85c30059a293227f6
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-11 10:02:12 +0900

    [level 3] Title: 길 찾기 게임, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
@@ -1,32 +1,31 @@
 import sys
 
-
 class Node:
     def __init__(self, id, x, y):
+        self.left = None
+        self.right= None
         self.id = id
         self.x = x
         self.y = y
-        self.left = None
-        self.right = None
         
     def __lt__(self, other):
-        if (self.y == other.y):
+        if self.y == other.y:
             return self.x < other.x
         return self.y > other.y
+            
     
-def addNode(parent, child):
+def linkNode(parent, child):
     if child.x < parent.x:
         if parent.left is None:
             parent.left = child
-        else: 
-            addNode(parent.left,child)
+        else:
+            linkNode(parent.left, child)
     else:
         if parent.right is None:
             parent.right = child
         else:
-            addNode(parent.right, child)
+            linkNode(parent.right, child)
 
-            
 def preorder(ans, node):
     if node is None:
         return
@@ -40,23 +39,28 @@ def postorder(ans, node):
     postorder(ans, node.left)
     postorder(ans, node.right)
     ans.append(node.id)
+    
 
 def solution(nodeinfo):
     sys.setrecursionlimit(1500)
-    size = len(nodeinfo)
-    nodelist = []
-    for i in range(size):
-        nodelist.append(Node(i+1, nodeinfo[i][0], nodeinfo[i][1]))
+    node_list = []
+    
+    for i in range(len(nodeinfo)):
+        node_list.append(Node(i+1, nodeinfo[i][0], nodeinfo[i][1]))
         
-    nodelist.sort()
+    node_list.sort()
     
-    root = nodelist[0]
+    root = node_list[0]
     
-    for i in range(1, size):
-        addNode(root, nodelist[i])
+    for i in range(1, len(nodeinfo)):
+        linkNode(root, node_list[i])
         
     answer = [[], []]
+    
     preorder(answer[0], root)
     postorder(answer[1], root)
     
     return answer
+    
+        
+    
