5aedcd1dd7d2052a71f64463acc152f0af87b95a
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-24 21:00:53 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.18 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -1,50 +1,50 @@
 from collections import deque
 
-def bfs(row, col, place):
-    que = deque()
-    que.append((row, col, 0))
-    direction = [(0,1),(0,-1),(1,0),(-1,0)]
-    visited = [[False] * 5 for _ in range(5)]
-    visited[row][col] = True
-    
-    while que:
-        cur_row, cur_col, cur_distance = que.popleft()
-        if cur_distance > 2:
-            continue
-        if cur_distance != 0 and place[cur_row][cur_col] == "P":
-            return False
-        
-        for r, c in direction:
-            next_row = cur_row + r
-            next_col = cur_col + c
-            
-            if next_row < 0 or next_row >= 5 or next_col < 0 or next_col >= 5 or visited[next_row][next_col]:
-                continue
-            if place[next_row][next_col] == "X":
-                continue
-            que.append((next_row, next_col, cur_distance + 1))
-            visited[next_row][next_col] = True
-            
-    return True
-
-def check(place):
-    n = len(place)
-    
-    for row in range(n):
-        for col in range(n):
-            if place[row][col] == "P":
-                if not bfs(row, col, place):
-                    return False
-    return True
 
 def solution(places):
     answer = []
-    
+
+    def bfs(row, col, place):
+        ROWS, COLS = len(place), len(place[0])
+
+        que = deque()
+        que.append((row, col, 0))
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        visited = [[False] * COLS for _ in range(ROWS)]
+
+        while que:
+            cur_row, cur_col, cur_distance = que.popleft()
+            visited[cur_row][cur_col] = True
+            if 0 < cur_distance <= 2 and place[cur_row][cur_col] == "P":
+                return False
+            for r, c in directions:
+                next_row, next_col = r + cur_row, c + cur_col
+
+                if next_row < 0 or next_row >= ROWS or next_col < 0 or next_col >= COLS:
+                    continue
+
+                if visited[next_row][next_col] or place[next_row][next_col] == "X":
+                    continue
+
+                que.append((next_row, next_col, cur_distance + 1))
+        return True
+
+    def checkPlace(place):
+        ROWS, COLS = len(place), len(place[0])
+
+        for row in range(ROWS):
+            for col in range(COLS):
+                if place[row][col] == "P":
+                    if not bfs(row, col, place):
+                        return False
+        return True
+
     for place in places:
-        if check(place):
+        if checkPlace(place):
             answer.append(1)
         else:
             answer.append(0)
-    
-    print(answer)
+
     return answer
+
+
