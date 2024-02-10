e20ce57c7c2fd7a409da0a7068df3ffcd7ce6c08
gygy <67543454+Gayun00@users.noreply.github.com>
2024-02-10 22:29:44 +0900

    Time: 241 ms (71.77%), Space: 18.9 MB (90.57%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,36 +2,30 @@ from collections import deque
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        ROWS, COLS = len(grid), len(grid[0])
+        ROWS,COLS = len(grid), len(grid[0])
         visited = [[False] * COLS for _ in range(ROWS)]
-        answer = 0
-        
-        def bfs(row, col):
+        count = 0
+        directions = [(1,0), (0,1), (-1,0),(0,-1)]
+
+        def countIslands(row, col):
             que = deque()
-            directions = [(0,1),(0,-1),(1,0),(-1,0)]
-            visited[row][col] = True
             que.append((row, col))
             
-
             while que:
                 cur_row, cur_col = que.popleft()
-
-                for r, c in directions:
-                    next_row = cur_row + r
-                    next_col = cur_col + c
+                
+                for row, col in directions:
+                    next_row, next_col = row + cur_row, col + cur_col
 
                     if next_row <0 or next_row >=ROWS or next_col < 0 or next_col >= COLS or visited[next_row][next_col] or grid[next_row][next_col] =="0":
                         continue
                     visited[next_row][next_col] = True
                     que.append((next_row, next_col))
-    
-        for row in range(ROWS):
-            for col in range(COLS):
-                if grid[row][col] == "1" and not visited[row][col]:
-                    bfs(row, col)
-                    answer += 1
-                    
-        return answer
-                
-            
         
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == "1" and not visited[i][j]:
+                    countIslands(i, j)
+                    count += 1
+        
+        return count
