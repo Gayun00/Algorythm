92bd83d5691887793dcf2cf9613e38a29b018b6f
gygy <67543454+Gayun00@users.noreply.github.com>
2024-02-10 14:49:43 +0900

    Time: 30 ms (91.71%), Space: 16.6 MB (59.68%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,18 +1,16 @@
-from collections import deque
-
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[0] * n for _ in range(m)]
-        directions = [(0, 1), (1, 0)]
-
+        grid = [[0] * n for _ in range(m)]
+        
         for i in range(m):
-            dp[i][0] = 1
-        for j in range(n):
-            dp[0][j] = 1
-            
-            
+            for j in range(n):
+                grid[i][0] = 1
+                grid[0][j] = 1
+                
         for i in range(1, m):
             for j in range(1, n):
-                dp[i][j] = dp[i-1][j] + dp[i][j-1]
-
-        return dp[-1][-1]   
+                grid[i][j] = grid[i-1][j] + grid[i][j-1]
+                
+                
+        return grid[m-1][n-1]
+        
