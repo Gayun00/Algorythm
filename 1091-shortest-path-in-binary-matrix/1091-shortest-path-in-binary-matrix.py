b3ba2c10cb2f4364d5f6e44de616a793c9feb61e
gygy <67543454+Gayun00@users.noreply.github.com>
2024-02-10 22:00:01 +0900

    Time: 393 ms (87.25%), Space: 16.8 MB (92.94%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -2,39 +2,35 @@ from collections import deque
 
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        ROWS, COLS = len(grid), len(grid[0])
+        n = len(grid)
         que = deque()
+        direction = [(0,1), (1,1), (1,0), (-1,-1), (0,-1), (-1,0), (-1,1), (1,-1)]
+        
         que.append((0, 0, 1))
-        directions = [(0,1),(0,-1),(1,1),(1,-1),(1,0),(-1,0),(-1,-1),(-1,1)]
-        distance = [[float('inf')] * COLS for _ in range(ROWS)]
-        distance[0][0] = 1
+        visited = [[float('inf')] * n for _ in range(n)]
+        visited[0][0] = 1
+        
         
         while que:
             cur_row, cur_col, cur_distance = que.popleft()
             if grid[cur_row][cur_col]:
                 return -1
             
-            for r, c in directions:
-                next_row = r + cur_row
-                next_col = c + cur_col
-                
+            for row,col in direction:
+                next_row, next_col = cur_row+row, cur_col+col
                 next_distance = cur_distance + 1
                 
-                if next_row < 0 or next_row >= ROWS or next_col < 0 or next_col >= COLS:
+                if next_row < 0 or next_row >= n or next_col < 0 or next_col >= n:
                     continue
                 if grid[next_row][next_col]:
                     continue
-                if distance[next_row][next_col] <= next_distance:
+                if visited[next_row][next_col] <= next_distance:
                     continue
-                distance[next_row][next_col] = next_distance
                 que.append((next_row, next_col, next_distance))
-                
-                
-        if distance[-1][-1] == inf:
+                visited[next_row][next_col] = next_distance
+                    
+        if visited[-1][-1] == inf:
             return -1
         else:
-            return distance[-1][-1]
-                
-                
-                
-            
+            return visited[-1][-1]
+        
