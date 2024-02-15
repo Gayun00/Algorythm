1d6f816777a9ad1af22d37eaf9a9ba265ff62872
gygy <67543454+Gayun00@users.noreply.github.com>
2024-02-15 18:56:33 +0900

    Time: 79 ms (14.99%), Space: 17.1 MB (84.07%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -2,22 +2,19 @@ import math
 
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        operators = ['+', '-', '*', '/']
+        stack = []
+        operators = {
+            '+': lambda a, b: a+b,
+            '-': lambda a, b: b-a,
+            '*': lambda a, b: a*b,
+            '/': lambda a, b: int(b/a),
+        }
         
-        def calcaulate(tokens):
-            if len(tokens) == 1:
-                return int(tokens[0])
-
-            for i, token in enumerate(tokens[:]):
-                if token in operators:
-                    result = 0
-                    if token == "/" and (int(tokens[i-2]) < 0 or int(tokens[i-1]) < 0):
-                        result = str(int(int(tokens[i-2]) / int(tokens[i-1])))
-                    else:
-                        result = str(math.floor(eval(tokens[i-2] + token + tokens[i-1])))
-                    tokens[i] = result
-                    del tokens[i-2:i]
-                    return calcaulate(tokens)
-
-        return calcaulate(tokens)
+        for token in tokens:
+            if token in operators:
+                stack.append(operators[token](stack.pop(), stack.pop()))
+            else:
+                stack.append(int(token))
+                
+        return stack[0]
         
